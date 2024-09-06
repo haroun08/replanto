07e4b225a1664c69ee421249b769219dd2309e7c
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-09-06 18:30:05 +0100

    ChatBot

--- lib/screens/home/views/widgets/AI assistance chatbot/chat_page.dart
+++ lib/screens/home/views/widgets/AI assistance chatbot/chat_page.dart
@@ -1,7 +1,9 @@
-import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
-import 'package:dash_chat_2/dash_chat_2.dart';
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+import 'message_widget.dart';
 import 'consts.dart';
 
 class ChatPage extends StatefulWidget {
@@ -12,101 +14,171 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  final _openAI = OpenAI.instance.build(
-    token: OPENAI_API_KEY,
-    baseOption: HttpSetup(
-      receiveTimeout: const Duration(seconds: 5),
-    ),
-    enableLog: true,
-  );
 
-  final ChatUser _currentUser = ChatUser(id: '1', firstName: 'Hussain', lastName: 'Mustafa');
-  final ChatUser _gptChatUser = ChatUser(id: '2', firstName: 'AI', lastName: 'Assistance');
+  late final GenerativeModel _model;
+  late final ChatSession _chatSession;
+  final FocusNode _textFieldFocus = FocusNode();
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+  bool _loading  = false;
 
-  List<ChatMessage> _messages = <ChatMessage>[];
+  final String apiKey = GEMINI_APi_KEY;
+
+  @override
+  void initState() {
+    super.initState();
+    _model = GenerativeModel(
+        model: 'gemini-pro',
+        apiKey: apiKey
+    );
+    _chatSession = _model.startChat();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Text(
-          'AI assistance',
-          style: TextStyle(color: Colors.white),
-        ),
+        title: const Text('replanto AI bot'),
       ),
-      body: DashChat(
-        currentUser: _currentUser,
-        messageOptions: const MessageOptions(
-          currentUserContainerColor: Colors.black,
-          containerColor: Colors.green,
-          textColor: Colors.white,
-        ),
-        onSend: (ChatMessage m) {
-          getChatResponse(m);
-        },
-        messages: _messages,
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount: _chatSession.history.length,
+                  itemBuilder: (context, index) {
+                    final Content content = _chatSession.history.toList()[index];
+                    final text = content.parts
+                        .whereType<TextPart>()
+                        .map<String>((e) => e.text)
+                        .join('');
+                    return MessageWidget(
+                      text: text,
+                      isFromUser: content.role == 'user',
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        autofocus: true,
+                        focusNode: _textFieldFocus,
+                        decoration: textFieldDecoration(),
+                        controller: _textController,
+                        onSubmitted: _sendChatMessage,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () => _sendChatMessage(_textController.text),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          if (_loading)
+            Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
       ),
     );
   }
 
-
-
-  Future<void> getChatResponse(ChatMessage m) async {
+  Future<void> _sendChatMessage(String message) async {
     setState(() {
-      _messages.insert(0, m);
+      _loading = true;
     });
 
-    List<Map<String, dynamic>> _messagesHistory = _messages.reversed.map((m) {
-      if (m.user == _currentUser) {
-        return {
-          'role': 'user',
-          'content': m.text,
-        };
-      } else {
-        return {
-          'role': 'assistant',
-          'content': m.text,
-        };
-      }
-    }).toList();
-
-    // Create the request for the GPT model
-    final request = ChatCompleteText(
-      model: Gpt40314ChatModel(), // Assuming you're using the GPT-4 model
-      messages: _messagesHistory,
-      maxToken: 200,
-    );
-
     try {
-      // Send the request and wait for the response
-      final response = await _openAI.onChatCompletion(request: request);
-      for (var element in response!.choices) {
-        if (element.message != null) {
-          setState(() {
-            _messages.insert(
-              0,
-              ChatMessage(
-                user: _gptChatUser,
-                createdAt: DateTime.now(),
-                text: element.message!.content,
-              ),
-            );
-          });
-        }
+      log("User message: $message");
+
+      final response = await _chatSession.sendMessage(Content.text(message));
+      final text = response.text;
+
+      if (text == null) {
+        log("API response error: No response from API", level: 900);
+        _showError('No response from API');
+        return;
+      } else {
+        log("AI response: $text", level: 800);
+        setState(() {
+          _loading = false;
+          _scrollDown();
+        });
       }
     } catch (e) {
-      print('Error during chat completion: $e');
+      log("Error sending message: $e", error: e, level: 1000);
+      _showError(e.toString());
       setState(() {
-        _messages.insert(
-          0,
-          ChatMessage(
-            user: _gptChatUser,
-            createdAt: DateTime.now(),
-            text: 'Error: Unable to fetch response. Please try again later.',
-          ),
-        );
+        _loading = false;
       });
+    } finally {
+      _textController.clear();
+      setState(() {
+        _loading = false;
+      });
+      _textFieldFocus.requestFocus();
     }
+  }
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+          (_) => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 750),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
+
+  InputDecoration textFieldDecoration() {
+    return InputDecoration(
+      contentPadding: const EdgeInsets.all(15),
+      hintText: 'Enter a prompt...',
+      border: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+    );
+  }
+
+  void _showError(String message) {
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Something went wrong"),
+          content: SingleChildScrollView(child: SelectableText(message)),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
+    );
   }
 }
